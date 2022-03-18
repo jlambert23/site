940bbb05c1b6fbb7e9ff7e36e65f6e15441f2bac
Jay <jb.lambert23@gmail.com>
2022-03-18 01:36:55 -0400

    eleventy playground

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+const { EleventyRenderPlugin } = require('@11ty/eleventy');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const markdownIt = require('markdown-it');
+const markdownItEmoji = require('markdown-it-emoji');
+
+module.exports = function (config) {
+  // css
+  config.addPassthroughCopy('./src/css');
+  config.addWatchTarget('./src/css/');
+
+  // plugins
+  config.addPlugin(EleventyRenderPlugin);
+  config.addPlugin(syntaxHighlight);
+
+  // markdown library config
+  const markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true,
+  }).use(markdownItEmoji);
+  config.setLibrary('md', markdownLibrary);
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+    },
+  };
+};
---
+++ src/index.md
@@ -0,0 +1,10 @@
+---
+layout: base.njk
+title: Mike's Super Short Show 🤙
+---
+
+```js
+function myFunction() {
+  return true;
+}
+```
